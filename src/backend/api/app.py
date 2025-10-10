eac0256af259690247b147a9c261599b1bea410f
DEV-ZERO-cloud <andresrubianomarrugo@gmail.com>
2025-10-10 16:01:25 -0500

    auth y otras cosas

--- src/backend/api/app.py
+++ src/backend/api/app.py
@@ -2,9 +2,8 @@ import os, httpx
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-#from backend.app.core.config import settings
-#from backend.app.core.middlewares import add_middlewares
-#from backend.app.logic.universal_controller_instance import universal_controller
+#from api.core.config import settings
+#from api.core.middlewares import add_middlewares
 from api.routes import (
     sesion_service,
     asistencia_sesion_service,
@@ -15,15 +14,20 @@ from api.routes import (
 )
 
 # Inicializar la aplicación FastAPI
-app = FastAPI()
+app = FastAPI(
+    title="AS_ASISTENCIAMOVIL",
+    description="Documentación automática con Swagger (FastAPI)",
+    version="1.0.0"
+)
 
 # Añadir middlewares globales
-    #add_middlewares(app)
+
+#add_middlewares(app)
 
 """app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4967",  # solo si aún pruebas en local
+        "http://localhost:8000",  # solo si aún pruebas en local
     ],
     allow_credentials=True,
     allow_methods=["*"],
