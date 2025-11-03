b0e8d01152d61642537fabb80e6f11a6d5276178
DEV-ZERO-cloud <andresrubianomarrugo@gmail.com>
2025-11-03 15:08:19 -0500

    https://www.youtube.com/watch?v=qQzdAsjWGPg

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'api/core/auth.dart';
+import 'dart:io';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+
   runApp(MyApp());
 }
 
